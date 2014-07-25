53dc1549cfe9a52b3dc6655aa9fb22dea295995f
Lycanite <rjnicholson1591@gmail.com>
2014-07-25 12:38:50 +0100

    1.7.2 Progress Custom food items should now have a description also.

--- java/lycanite/lycanitesmobs/api/item/ItemCustomFood.java
+++ java/lycanite/lycanitesmobs/api/item/ItemCustomFood.java
@@ -1,10 +1,17 @@
 package lycanite.lycanitesmobs.api.item;
 
+import java.util.List;
+
 import lycanite.lycanitesmobs.AssetManager;
 import lycanite.lycanitesmobs.LycanitesMobs;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemFood;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
+import net.minecraft.util.StatCollector;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
@@ -29,6 +36,28 @@ public class ItemCustomFood extends ItemFood {
 	public ItemCustomFood(String setItemName, String setDomain, int feed, float saturation) {
 		this(setItemName, setDomain, setItemName.toLowerCase(), feed, saturation);
 	}
+    
+    
+	// ==================================================
+	//                      Info
+	// ==================================================
+    @Override
+    public void addInformation(ItemStack itemStack, EntityPlayer entityPlayer, List textList, boolean par4) {
+    	String description = this.getDescription(itemStack, entityPlayer, textList, par4);
+    	if(!"".equalsIgnoreCase(description) && !("item." + this.itemName + ".description").equals(description)) {
+    		FontRenderer fontRenderer = Minecraft.getMinecraft().fontRenderer;
+    		List formattedDescriptionList = fontRenderer.listFormattedStringToWidth(description, 64);
+    		for(Object formattedDescription : formattedDescriptionList) {
+    			if(formattedDescription instanceof String)
+    				textList.add("\u00a7a" + (String)formattedDescription);
+    		}
+    	}
+    	super.addInformation(itemStack, entityPlayer, textList, par4);
+    }
+    
+    public String getDescription(ItemStack itemStack, EntityPlayer entityPlayer, List textList, boolean par4) {
+    	return StatCollector.translateToLocal("item." + this.itemName + ".description");
+    }
 	
 	
     // ==================================================

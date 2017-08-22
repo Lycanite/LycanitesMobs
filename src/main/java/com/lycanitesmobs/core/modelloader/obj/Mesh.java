a2b45148264b69ded016c76f02a850c21b6a876e
Lycanite <rjnicholson1591@gmail.com>
2017-08-22 18:30:08 +0100

    Normals Cache
    When rendering models, the normals are now cached saving a lot of CPU time!
    Fixed Ioray dropping Life Drain Charges instead of Water jet Charges.

--- src/main/java/com/lycanitesmobs/core/modelloader/obj/Mesh.java
+++ src/main/java/com/lycanitesmobs/core/modelloader/obj/Mesh.java
@@ -1,9 +1,11 @@
 package com.lycanitesmobs.core.modelloader.obj;
 
 
+import javax.vecmath.Vector3f;
+
 public class Mesh
 {
-
     public int[] indices;
     public Vertex[] vertices;
+    public Vector3f[] normals;
 }

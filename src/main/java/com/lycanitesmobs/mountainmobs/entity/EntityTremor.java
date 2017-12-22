2f70a7f25306c572c8cd29c76a2cb9dd09562cfc
Lycanite <rjnicholson1591@gmail.com>
2017-12-22 17:38:43 +0000

    1.17.4.0 Explosive!
    ============================================================
    Lycanites Mobs Update: Explosive! - Version 1.17.4.0 for Minecraft 1.12.2
    Configs older than 1.17.3.2 will be reset.
    ============================================================
    
    --------------------
    New Features:
    --------------------
    New Mob Added: Tremor - The Quake Elemental which spawns from explosions!
    
    
    --------------------
    Major Fixes:
    --------------------
    Fixed a bug where some spawn type weren't unloaded after the spawners reload command was used causing them to duplicate every time!
    Fixed a bug where Nethersoul Sigil spawned Nethersouls didn't save their explosion state allowing them to become permanent minions!
    
    
    --------------------
    Config Changes:
    --------------------
    Added "season" value to the Date Spawn Condition to check for seasonal times like "easter", the seasonal events have been changed to use these internal checks instead.
    Added checks for Valentines, Easter and Midsummer for future events and gift items to be added later.
    Added entity checks to the explosion spawn type, this is used to stop Tremors from spawning more Tremors with their explosive attacks.
    
    
    --------------------
    Minor Fixes:
    --------------------
    Fixed a bug where Nethersouls had no fire particles at all.

--- src/main/java/com/lycanitesmobs/mountainmobs/entity/EntityTremor.java
+++ src/main/java/com/lycanitesmobs/mountainmobs/entity/EntityTremor.java
@@ -67,7 +67,6 @@ public class EntityTremor extends EntityCreatureTameable implements IMob, IGroup
         this.targetTasks.addTask(0, new EntityAITargetOwnerRevenge(this));
         this.targetTasks.addTask(1, new EntityAITargetOwnerAttack(this));
         this.targetTasks.addTask(2, new EntityAITargetRevenge(this).setHelpCall(true));
-        this.targetTasks.addTask(3, new EntityAITargetAttack(this).setTargetClass(EntitySilverfish.class));
         this.targetTasks.addTask(4, new EntityAITargetAttack(this).setTargetClass(EntityPlayer.class));
         this.targetTasks.addTask(4, new EntityAITargetAttack(this).setTargetClass(EntityVillager.class));
         this.targetTasks.addTask(6, new EntityAITargetOwnerThreats(this));
